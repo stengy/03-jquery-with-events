39d3d7d13b3cd2b1c7276c58d10f0aa82fe7e1a2
Dustin Byers <dbyers84@gmail.com>
2017-05-10 18:07:00 -0700

    fixed the nav bar selectors

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -76,7 +76,11 @@ articleView.handleMainNav = function() {
   //       single .tab-content section that is associated with the clicked .tab element.
   //       So: You need to dynamically build a selector string with the correct ID, based on the
   //       data available to you on the .tab element that was clicked.
-
+  $('.tab').on('click', function() {
+    var $tabSelected = $(this).data('content')
+    $('.tab-content').hide()
+    $('#' + $tabSelected).fadeIn(500)
+  })
 
   $('.main-nav .tab:first').click(); // Let's now trigger a click on the first .tab element, to set up the page.
 };
