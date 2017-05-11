c476869950182a65c16bc70437de5a801295505b
Dustin Byers <dbyers84@gmail.com>
2017-05-10 17:24:10 -0700

    show all articles other than the template

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -40,14 +40,14 @@ articleView.handleAuthorFilter = function() {
       // TODO: If the select box was changed to an option that has a value, we need to hide all the articles,
       //       and then show just the ones that match for the author that was selected.
       //       Use an "attribute selector" to find those articles, and fade them in for the reader.
-      // var $authorToShow = $(this).val()
-      console.log(this.value)
+
       $('article').hide()
       $('article[data-author="' + $(this).val() + '"]').fadeIn(500)
     } else {
       // TODO: If the select box was changed to an option that is blank, we should
       //       show all the articles, except the one article we are using as a template.
-
+      $('article').show()
+      $('.template').hide()
     }
     $('#category-filter').val('');
   });
